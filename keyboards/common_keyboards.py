e61df0d0bf71deff2f04e4c6b87a42c635e90d2e
Nonmon <tvoy@email.com>
2025-07-26 18:35:54 +0200

    likes

--- keyboards/common_keyboards.py
+++ keyboards/common_keyboards.py
@@ -11,7 +11,7 @@ def get_main_menu_keyboard() -> ReplyKeyboardMarkup:
         [KeyboardButton(text="Мои продажи 📋")],
         [KeyboardButton(text="Получить этикетку 🏷️")],
         [KeyboardButton(text="Сводка 📈")],
-        [KeyboardButton(text="⭐️ Показать счётчик лайков"),
+        [KeyboardButton(text="⭐️ Показать счётчик лайков")],
         [KeyboardButton(text="❤️ Сбросить счётчик лайков")]
     ]
     keyboard = ReplyKeyboardMarkup(keyboard=buttons, resize_keyboard=True)
